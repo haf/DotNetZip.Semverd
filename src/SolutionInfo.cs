333f171e93d04bd0bb2d9170fa1ac62117aaa423
Christoph Fink <fink.christoph@gmail.com>
2016-05-02 12:37:36 +0200

    bump version to 1.9.9

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,6 +2,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-[assembly: AssemblyVersion("1.9.8")]
-[assembly: AssemblyFileVersion("1.9.8")]
-[assembly: AssemblyInformationalVersion("1.9.8.5a841a")]
+[assembly: AssemblyVersion("1.9.9")]
+[assembly: AssemblyFileVersion("1.9.9")]
+[assembly: AssemblyInformationalVersion("1.9.9.e09c70d")]
