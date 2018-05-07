f7e1c3547540bf43bc6819dac489e6b107b1c07a
silversj <jps@margreta.com>
2018-05-07 15:34:03 -0400

    Bump version to 1.11.0 (#124)

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,6 +2,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-[assembly: AssemblyVersion("1.10.1")]
-[assembly: AssemblyFileVersion("1.10.1")]
-[assembly: AssemblyInformationalVersion("1.10.1.000000")]
+[assembly: AssemblyVersion("1.11.0")]
+[assembly: AssemblyFileVersion("1.11.0")]
+[assembly: AssemblyInformationalVersion("1.11.0.000000")]
