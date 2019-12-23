c449fb2c641104f84259a72bf7a0bd474a0c09d1
Mikhail Umnov <darkms5@gmail.com>
2019-12-23 22:16:45 +0700

    Bump version

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-[assembly: AssemblyVersion("1.13.4")]
-[assembly: AssemblyFileVersion("1.13.4")]
-[assembly: AssemblyInformationalVersion("1.13.4.000000")]
+[assembly: AssemblyVersion("1.13.5")]
+[assembly: AssemblyFileVersion("1.13.5")]
+[assembly: AssemblyInformationalVersion("1.13.5.000000")]
