fc2080acf4d3477c087c7f3fe013cf379783a742
darkms <darkms5@gmail.com>
2020-05-21 23:34:17 +0700

    Release 1.13.8 to nuget

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-[assembly: AssemblyVersion("1.13.7")]
-[assembly: AssemblyFileVersion("1.13.7")]
-[assembly: AssemblyInformationalVersion("1.13.7.000000")]
+[assembly: AssemblyVersion("1.13.8")]
+[assembly: AssemblyFileVersion("1.13.8")]
+[assembly: AssemblyInformationalVersion("1.13.8.000000")]
